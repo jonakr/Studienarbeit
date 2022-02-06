f7e22d97bb0a06fb9fa6104bc64cf4356b0ce8fd
Jona <krumrein.jona@gmail.com>
2022-02-06 14:21:45 +0100

    switched contents of export data parsers to match app

--- matlab/WithSamplesParser.m
+++ matlab/WithSamplesParser.m
@@ -8,10 +8,38 @@ classdef WithSamplesParser
     methods
         function obj = WithSamplesParser(path2file)
             obj.path = path2file;
-
-            
-
             obj.data = load(path2file);
+
+            obj = obj.adjustFrequencyData();
+        end
+
+        function plotToAxes(obj, setup, UIAxes, selectedVar)
+            idx = find(setup.result.short == selectedVar);
+            
+            index = setup.result.index(idx);
+            description = setup.result.description(idx);
+            unit = setup.result.unit(idx);
+            type = setup.result.type(idx);
+
+            plot(UIAxes, obj.data.Res.HRV.(type).(index));
+            UIAxes.Title.String = selectedVar;
+            UIAxes.Subtitle.String = description;
+            UIAxes.YLabel.String = unit;
+            UIAxes.XLabel.String = 'time in min';
+        end
+
+        function obj = adjustFrequencyData(obj)
+            % adjust the frequency struct to match plotting scheme
+            % warning! this deletes the other measurements if not copied
+            % TODO: adjust scheme of every variable
+            
+            frequencyData = obj.data.Res.HRV.Frequency;
+            LFHFpower = double.empty(length(frequencyData), 0);
+
+            for idx = 1:length(frequencyData)
+                LFHFpower(idx) = frequencyData(idx).Welch.('LF_HF_power');
+            end
+            obj.data.Res.HRV.Frequency = struct('LF_HF_power', LFHFpower);
         end
     end
 end
