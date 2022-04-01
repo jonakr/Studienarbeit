6abc9d45b5506cde996cd2c268786d29a52a270e
Jona <krumrein.jona@gmail.com>
2022-04-01 13:05:06 +0200

    fixed csv creation

--- matlab/Create_CSV.m
+++ matlab/Create_CSV.m
@@ -1,15 +1,17 @@
 function Create_CSV(sampleLength,fileLength,sampleShift,filename)
-    csvstr = filename + "," + "1" + ",";
+    export = [filename,"1"];
     color = " # 0 255 0";
-
     starttime = 0;
-    endtime = sampleLength*60;
+    endtime = sampleLength * 60;
 
-    for i =1:(fileLength + 1 - sampleLength)
-        csvstr = strcat(csvstr,"Sample",int2str(i),color,",",int2str(starttime),",",int2str(endtime),",");
-        endtime = endtime + (sampleShift*60);
-        starttime = starttime + (sampleShift*60); 
+    for i = 1:(floor(fileLength/sampleShift))
+        if (endtime > fileLength * 60)
+            break;
+        end
+        export = [ export "Sample" + i + color starttime endtime ];
+        endtime = endtime + (sampleShift * 60);
+        starttime = starttime + (sampleShift * 60);
     end
-    writematrix(csvstr, "Kubios_Samples.csv");
+    writematrix(export,'Kubios_Samples.csv','Delimiter','comma')
 end
 
