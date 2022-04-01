8a4084141b33c5131f81cad04ff18970fbd2bea4
Jona <krumrein.jona@gmail.com>
2022-04-01 14:51:13 +0200

    refactored and added sample start option

--- matlab/createCSV.m
+++ matlab/createCSV.m
@@ -1,8 +1,10 @@
-function Create_CSV(sampleLength,fileLength,sampleShift,filename)
+function createCSV(sampleStart, sampleLength, fileLength, ...
+    sampleShift, filename)
+
     export = [filename,"1"];
     color = " # 0 255 0";
-    starttime = 0;
-    endtime = sampleLength * 60;
+    starttime = sampleStart * 60;
+    endtime = starttime + sampleLength * 60;
 
     for i = 1:(floor(fileLength/sampleShift))
         if (endtime > fileLength * 60)
