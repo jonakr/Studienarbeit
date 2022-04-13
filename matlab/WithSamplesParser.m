79f0e9dfea518084cdcdc164f02ad1cb3c551701
Jona <krumrein.jona@gmail.com>
2022-04-13 15:22:36 +0200

    new plot function with new data and setup structure

--- matlab/WithSamplesParser.m
+++ matlab/WithSamplesParser.m
@@ -22,20 +22,35 @@ classdef WithSamplesParser
             obj.data.Res.HRV.Param.Nbr_Segment * obj.sampleLength;
         end
 
-        function plotToAxes(obj, setup, UIAxes, selectedVar)
-            idx = find(setup.result.short == selectedVar);
+        function plotToAxes(obj, setup, UIAxes, selectedVar, selectedGroup)
             
-            index = setup.result.index(idx);
-            description = setup.result.description(idx);
-            unit = setup.result.unit(idx);
-            type = setup.result.type(idx);
+            if selectedGroup == "Time-Domain"
+                setuplink = setup.variablesStruct.timeDomain;
+                datalink = obj.data.Res.HRV.Statistics;
+            elseif selectedGroup =="Frequency-Domain FFT spectrum"
+                setuplink = setup.variablesStruct.frequencyDomain;
+                datalink = obj.data.Res.HRV.Frequency.Welch;
+            elseif selectedGroup =="Frequency-Domain AR spectrum"
+                setuplink = setup.variablesStruct.frequencyDomain;
+                datalink = obj.data.Res.HRV.Frequency.AR;
+            elseif selectedGroup =="Nonlinear"
+               setuplink = setup.variablesStruct.nonlinear;
+               datalink = obj.data.Res.HRV.NonLinear;
+            end
+
+            idx = find(contains({setuplink(:).index}, selectedVar));
+            
+            short = setuplink(idx).short;
+            description = setuplink(idx).description;
+            unit = setuplink(idx).unit;
+            type = setuplink(idx).type;
 
             % plot data and adjust axis
-            scatter(UIAxes, obj.minutesArray, obj.data.Res.HRV.(type).(index), ...
+            scatter(UIAxes, obj.minutesArray, datalink.(selectedVar), ...
                                                 'MarkerEdgeColor',[0 .5 .5],...
                                                 'MarkerFaceColor',[0 .7 .7],...
                                                 'LineWidth',1.5);
-            UIAxes.Title.String = selectedVar;
+            UIAxes.Title.String = short;
             UIAxes.Subtitle.String = description;
             UIAxes.YLabel.String = unit;
             UIAxes.XLabel.String = 'minutes';
@@ -45,6 +60,9 @@ classdef WithSamplesParser
         function obj = adjustData(obj)
             % adjust the different data structs to match plotting scheme
             % warning! this deletes the other measurements if not copied
+
+            % TODO: fix units e.g. RMSSD needs to be milliseconds
+            %       add missing variables e.g. AC DC
        
             % adjust statistics data
             obj.data.Res.HRV.Statistics = tableConverter(obj.data.Res.HRV.Statistics);
@@ -64,9 +82,20 @@ classdef WithSamplesParser
             cleanCorDim = nestedTableConverter(obj.data.Res.HRV.NonLinear, 'CorDim', {'log_r', 'log_C', 'th'}); 
             % get nested RPA data
             cleanRPA = nestedTableConverter(obj.data.Res.HRV.NonLinear, 'RPA', {'RP', 'Lhist'});
-            names = [fieldnames(cleanNotNested); fieldnames(cleanDFA); fieldnames(cleanCorDim); fieldnames(cleanRPA)];
+            % get nested MSE data
+            tempMatrix = [];
+            cleanMSE = struct();
+            for idx = 1:length(obj.data.Res.HRV.NonLinear)
+                tempMatrix = cat(1, tempMatrix, obj.data.Res.HRV.NonLinear(idx).MSE);
+            end
+            tempMatrix = tempMatrix';
+            for id = 1:size(tempMatrix, 1)
+                [cleanMSE.(['MSE' num2str(id)])] = tempMatrix(id,:);
+            end
+            % concat all structs
+            names = [fieldnames(cleanNotNested); fieldnames(cleanDFA); fieldnames(cleanCorDim); fieldnames(cleanRPA); fieldnames(cleanMSE)];
             obj.data.Res.HRV.NonLinear = cell2struct([struct2cell(cleanNotNested); ...
-                struct2cell(cleanDFA); struct2cell(cleanCorDim); struct2cell(cleanRPA)], names, 1);
+                struct2cell(cleanDFA); struct2cell(cleanCorDim); struct2cell(cleanRPA); struct2cell(cleanMSE)], names, 1);
 
         end
     end
